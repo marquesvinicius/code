843aa59d4bec3c928ab94f4431ef477dd829f468
Marques Vinícius <marquesviniciusmelomartins@gmail.com>
2022-07-19 21:13:48 -0300

    node e npm

---
+++ npm/Artster/index.js
@@ -0,0 +1,12 @@
+const figlet = require('figlet');
+const colors = require('colors');
+
+figlet('Hello World', function (err, data) {
+    if (err) {
+        console.log('something went wrong...');
+        console.dir(err);
+        return;
+    }
+
+    console.log(data.rainbow);
+})
---
+++ require/math.js
@@ -0,0 +1,23 @@
+const add = (x,y) => x + y;
+
+const PI = 3.14;
+
+const square = x => x * x;
+
+exports.add = add;
+exports.PI = PI;
+exports.square = square;
+
+
+// const math = {
+//     add: add,
+//     PI: PI,
+//     square: square;
+// }
+
+// module.exports = math; 
+
+
+// module.exports.add = add;
+// module.exports.PI = PI;
+// module.exports.square = square;
